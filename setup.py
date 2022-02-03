bbb3e8687203d411aa3b104781a906d42f65b184
danx0r <dan.miller@eye0.com>
2022-02-03 23:20:35 +0000

    tested

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 setup(
     name="burst_tools",
     version="0.1.2",
-    packages=find_packages(),
+    install_requires=[
+        "pynvml"
+    ],
     python_requires='>=3',
-    include_package_data=True
 )
