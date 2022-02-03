abde80e4ddf7834742b169c2dbf1b735269bc454
danx0r <dan.miller@eye0.com>
2022-02-03 19:51:09 +0000

    check devices

--- gpumem.py
+++ gpumem.py
@@ -14,5 +14,8 @@ if __name__ == "__main__":
     dev = 0
     if len(sys.argv) > 1:
         dev = int(sys.argv[1])
+    devs = nvmlDeviceGetCount()
+    if dev >= devs:
+        raise Exception(f"No such device; {devs} devices available")
     used, free, tot = mem(dev)
     print (f"{used/tot:.3%} {used/1000000:.3f}mb out of {tot/1000000:.3f}mb free: {free/1000000:.3f}mb")
