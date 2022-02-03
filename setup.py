b7791ddf9b454e3bfcfcabc5f575e523ef01b98e
danx0r <dan.miller@eye0.com>
2022-02-03 22:58:11 +0000

    proper Pip

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-    name="pyweb",
-    version="0.1.0",
+    name="burst_tools",
+    version="0.1.2",
     packages=find_packages(),
     python_requires='>=3',
     include_package_data=True
