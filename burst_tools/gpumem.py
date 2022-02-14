f11830092bf3f10f63b7cb4061b90a722f70ff04
danx0r <dan.miller@eye0.com>
2022-02-14 13:42:47 -0800

    graceful fails

--- burst_tools/gpumem.py
+++ burst_tools/gpumem.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-import sys
-import argparse
 from pynvml import *
 
-nvmlInit()
+try:
+    nvmlInit()
+    NVML = True
+except:
+    NVML = False
+
+def enabled():
+    return NVML
 
 #get mem stats
 def mem(dev=0):
@@ -28,10 +33,13 @@ def least_used():
     return best, freest
 
 if __name__ == "__main__":
-    best, free = least_used()
-    print (f"dev {best} has most free memory: {free/1000000:.3f}mb")
-    devs = nvmlDeviceGetCount()
-    print (f"{devs} GPU devices found")
-    for dev in range(devs):
-        used, free, tot = mem(dev)
-        print (f"device: {dev} used: {used/tot:.3%} {used/1000000:.3f}mb out of {tot/1000000:.3f}mb free: {free/1000000:.3f}mb")
+    if not NVML:
+        print ("NVML driver not available (no gpu?)")
+    else:
+        best, free = least_used()
+        print (f"dev {best} has most free memory: {free/1000000:.3f}mb")
+        devs = nvmlDeviceGetCount()
+        print (f"{devs} GPU devices found")
+        for dev in range(devs):
+            used, free, tot = mem(dev)
+            print (f"device: {dev} used: {used/tot:.3%} {used/1000000:.3f}mb out of {tot/1000000:.3f}mb free: {free/1000000:.3f}mb")
